ed20a5577feaee78ecf0b1eee34de9d7942f5a7c
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2023-12-11 11:12:39 +0500

    add: Messages uchun module va controller ochib oldim

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-messages.dto';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
